7d8bd84ec5c1b01a0b3da34dc213b4946b27e565
alle0017 <allegretti0017@gmail.com>
2024-06-23 10:34:44 +0200

    downloads

--- popup/index.js
+++ popup/index.js
@@ -9,38 +9,78 @@ const openSidebar = async ( title, text ) => {
         }catch{
             response = false;
         }
-        console.log( response );
     }
+    window.close();
 }
+/**
+ * 
+ * @param {string} title 
+ * @param {string} text 
+ * @returns 
+ */
 const createCard = ( title, text )=> {
     const card = document.createElement('div');
     const trash = document.createElement('img');
+    const cloud = document.createElement('img');
+    const wrapper = document.createElement('ul');
+    const item1 = document.createElement('li');
+    const item2 = document.createElement('li');
+    const item3 = document.createElement('li');
 
-    card.innerHTML = title;
-    card.classList.add('card');
-    card.style.width = '200px';
-    card.style.minWidth = '200px';
+    let padding = 0;
+
+    if( title.length < 20 )
+        padding = ( 20 - title.length )*10;
+
+    wrapper.append( item1, item2, item3 );
+
+    item1.style.marginRight = `${padding + 10}px`;
+    item1.style.paddingLeft = '0 !important';
+    item1.style.marginLeft = '0 !important';
+    item1.style.fontSize = '16px';
+    item1.innerHTML = title.length > 20? `${title.substring(0, 17)}...`: title;
+    item2.append( cloud );
+    item3.append( trash );
+
+    card.style.width = '250px';
+    card.style.minWidth = '250px';
     card.style.height = '50px'; 
-    card.style.marginTop = '5%';
-    card.style.marginRight = '20px';
     card.style.paddingLeft = '5%';
     card.style.paddingRight = '5%';
-    card.style.paddingTop = '15%';
+    card.style.paddingTop = '5%';
+    card.style.marginTop = '2%';
     card.style.backgroundColor = 'var(--secondary-color)';
     card.style.borderRadius = '5px';
+    card.title = text.substring( 0, 100 );
     card.addEventListener( 'click', () => openSidebar( title, text ) )
 
     trash.src = '../icons/trash.svg';
-    trash.style.position = 'absolute';
-    trash.style.right = '20px';
     trash.width = '20';
     trash.height = '20';
+    trash.classList.add('icon-button');
+    trash.title = 'Delete';
     trash.addEventListener('click', async e =>{
         e.stopPropagation();
         card.remove();
         await browser.storage.local.remove(title);
     })
-    card.append(trash);
+
+    cloud.src = '../icons/cloud.svg';
+    cloud.width = '20';
+    cloud.height = '20';
+    cloud.classList.add('icon-button');
+    cloud.title = 'Download';
+    cloud.addEventListener( 'click', async ()=>{
+        e.stopPropagation();
+        const blob = new Blob( [text], { type: 'text/plain' } );
+
+        await browser.downloads.download({
+            url: window.URL.createObjectURL( blob ),
+            saveAs: true,
+        });
+    })
+
+    card.append( wrapper );
 
     return card;
 }
--- sidebar/index.js
+++ sidebar/index.js
@@ -1,3 +1,22 @@
+const saveToast = ()=>{
+    const toast = document.createElement('div');
+
+    toast.innerHTML = 'Saved!'
+    toast.style.width = '200px';
+    toast.style.height = '30px';
+    toast.style.fontSize = '20px';
+    toast.style.color = '#000';
+    toast.style.paddingLeft = '15%';
+    toast.style.borderRadius = '2px';
+    toast.style.backgroundColor = '#8aff9add';
+    toast.style.position = 'absolute';
+    toast.style.top = '0';
+    toast.style.left = '30%';
+
+    document.body.appendChild(toast);
+
+    setTimeout( ()=> toast.remove(), 3000 );
+}
 browser.runtime.onMessage.addListener(  ( request, sender, sendResponse )=>{
     if( !request || !('text' in request) || !('title' in request) )
         return;
@@ -9,9 +28,25 @@ document.getElementById('save').addEventListener( 'click', async ()=>{
     const title = document.getElementById('title').value;
     const text = document.getElementById('text').value;
 
-    console.log( title, text )
+    if( !text || !title )
+        return;
+
     await browser.storage.local.set({ [title]: text });
+    saveToast();
+
     try{
         await browser.runtime.sendMessage({ title, text });
-    }catch(e){}
+    }catch(e){
+        console.error(e);
+    }
+});
+document.getElementById('download').addEventListener( 'click', async ()=>{
+
+    const text = document.getElementById('text').value;
+    const blob = new Blob( [text], { type: 'text/plain' } );
+
+    await browser.downloads.download({
+        url: window.URL.createObjectURL( blob ),
+        saveAs: true,
+    });
 });
